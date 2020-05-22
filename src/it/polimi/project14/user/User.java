3f2752cfae7bee90a0faf8665d25e71a594a8352
Alessio Spagnolo <alessio.spanish@mail.polimi.it>
2020-05-22 19:33:09 +0200

    RMI implementation and new class diagram allignament

--- src/it/polimi/project14/user/User.java
+++ src/it/polimi/project14/user/User.java
@@ -1,33 +1,47 @@
 package it.polimi.project14.user;
 
 import java.util.Set;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
 import it.polimi.project14.common.Event;
 import it.polimi.project14.common.SearchFilter;
-import it.polimi.project14.server.Server;
+import it.polimi.project14.server.EventStorage;
 
-public class User implements NotificationManager {
-	
+public class User {
+
 	private Set<String> favoriteCaps;
-	
+
 	public void setFavotiteCaps(Set<String> favoriteCaps) {
-        this.favoriteCaps = favoriteCaps;
-    }
-	
-	public Set<Event> getUrgentEvents() throws SQLException{
+		this.favoriteCaps = favoriteCaps;
+	}
+
+	public Set<Event> getUrgentEvents() throws Exception {
 		SearchFilter searchFilter = new SearchFilter();
 		searchFilter.setCapList(favoriteCaps);
 		searchFilter.setExpectedSince(LocalDate.now().atStartOfDay());
 		searchFilter.setMaxSeverity(true);
-		Server server = new Server();
-		return server.getEvents(searchFilter); //RMI
+		try {
+			EventStorage eventStorage = (EventStorage) Naming.lookup("EVENT_STORAGE");
+			return eventStorage.getEvents(searchFilter); // RMI
+		} catch (SQLException e) {
+			throw new Exception(e.getMessage());
+		} catch (RemoteException e) {
+			throw new Exception(e.getMessage());
+		}
 	}
-	
-	public Set<Event> searchEvents(SearchFilter searchFilter) throws SQLException{
-		Server server = new Server();
-		return server.getEvents(searchFilter); //RMI
+
+	public Set<Event> searchEvents(SearchFilter searchFilter) throws Exception {
+		try {
+			EventStorage eventStorage = (EventStorage) Naming.lookup("EVENT_STORAGE");
+			return eventStorage.getEvents(searchFilter); // RMI
+		} catch (SQLException e) {
+			throw new Exception(e.getMessage());
+		} catch (RemoteException e) {
+			throw new Exception(e.getMessage());
+		}
 	}
-	
+
 }
