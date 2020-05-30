ce7bfa97ea91b904b19801fa9bca08d8d2df6866
Alessio Spagnolo <alessio.spanish@mail.polimi.it>
2020-05-30 18:21:00 +0200

    User main added

--- Java/User/src/it/polimi/project14/CivilProtectionUser.java
+++ Java/User/src/it/polimi/project14/CivilProtectionUser.java
@@ -1,31 +1,15 @@
 package it.polimi.project14;
 
+import java.util.Set;
+
+import it.polimi.project14.user.Caps;
+import it.polimi.project14.user.User;
+import it.polimi.project14.user.gui.FrameCivilProtectionUser;
+
 public class CivilProtectionUser {
     public static void main(String[] args) {
-        try {
-            System.out.println("User!");
-            // Create User and GUI objects and pass data between
-
-            // // Test
-            // System.setProperty("java.security.policy", "policy.all");
-            // String urlServer = "rmi://localhost:6666/";
-            // EventStorage eventStorage = (EventStorage) Naming.lookup(urlServer + "EVENT_STORAGE");
-            // System.out.println("testing...");
-            // Event e = new Event();
-            // e.setEventId(3);
-            // e.setCap("91026");
-            // e.setMessage("Test");
-            // e.setKind("Terremoto");
-            // e.setExpectedAt(LocalDateTime.now());
-            // e.setSeverity(10);
-            // e.setSourceId(3);
-            // e.setStatus(EventStatus.OCCURED);
-            // System.out.println("testing event created");
-            // Set<Event> eventsList = new HashSet<Event>();
-            // eventsList.add(e);
-            // eventStorage.storeEvents(eventsList);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        Set<String> comoCaps = Caps.narrow("Como", null);
+        User user = new User(comoCaps);
+        FrameCivilProtectionUser userGui = new FrameCivilProtectionUser(user);
     }
 }
