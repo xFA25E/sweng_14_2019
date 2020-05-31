29df44c8f50e1eda03895acc061c164e52246e7a
Valeriy Litkovskyy <5009900-zae5quee2eequ6jieJaeh7chie6zie@users.noreply.gitlab.com>
2020-05-31 19:40:19 +0200

    added caps tests

--- Java/Tests/src/it/polimi/project14/CivilProtectionTests.java
+++ Java/Tests/src/it/polimi/project14/CivilProtectionTests.java
@@ -8,13 +8,15 @@ import org.junit.runner.notification.Failure;
 import it.polimi.project14.common.EventTests;
 import it.polimi.project14.common.SearchFilterTests;
 import it.polimi.project14.server.EventStorageImplTests;
+import it.polimi.project14.user.CapsTests;
 
 public class CivilProtectionTests {
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(
             EventTests.class,
             SearchFilterTests.class,
-            EventStorageImplTests.class
+            EventStorageImplTests.class,
+            CapsTests.class
         );
 
         for (Failure failure : result.getFailures()) {
