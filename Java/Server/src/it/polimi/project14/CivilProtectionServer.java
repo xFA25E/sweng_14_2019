8a61c1d0cf48dd9d7856db90581e7190c5451d43
Alessio Spagnolo <alessio.spanish@mail.polimi.it>
2020-06-01 15:15:05 +0200

    CivilProtectionServer Main debugging statement removed

--- Java/Server/src/it/polimi/project14/CivilProtectionServer.java
+++ Java/Server/src/it/polimi/project14/CivilProtectionServer.java
@@ -13,14 +13,12 @@ public class CivilProtectionServer {
             // Rebind instead of bind cause if it find something whit same
             // name it replace with this new object.(decrease bugs)
             System.setProperty("java.security.policy", "policy.all");
-            System.out.println(System.getProperty("java.security.policy"));
             Remote remoteEventStorage = new EventStorageImpl();
             LocateRegistry.createRegistry(6666);
             Naming.rebind("rmi://localhost:6666/EVENT_STORAGE", remoteEventStorage);
-            System.out.println("3");
         } catch (Exception e) {
             System.out.println("RMI error: " + e.getMessage());
         }
-        System.out.println("Starting server");
+        System.out.println("Server is running");
     }
 }
